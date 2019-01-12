b201f45cfb6f98be5801bc2dfc178f9da2dd7da3
MingyuSung <123msn@nexon.co.kr>
2019-01-12 15:49:44 +0900

    페이징 완료
    Signed-off-by: MingyuSung <123msn@nexon.co.kr>

---
+++ src/main/resources/static/js/pagination.js
@@ -0,0 +1,106 @@
+
+var pagination = {
+
+    limit : 0,
+    row : 0,
+    createPage : function(pageableCount){
+        totalPageCount = pageableCount;
+        var currentPageLimit = totalPageCount / pagination.row;
+
+        var source = $('#page-template').html();
+        var template = Handlebars.compile(source);
+
+        if (currentPageLimit > 0){
+            if (currentPageLimit <= pagination.limit){
+                var html = template({start:1, end:currentPageLimit});
+                $('#pageSection').empty().append(html);
+
+                $('.page-item.prev').addClass("disabled");
+                $('.page-item.next').addClass("disabled");
+
+                $(".page-item.item:first").addClass("active");
+            } else {
+                var html = template({start:1,end:pagination.limit});
+                $('#pageSection').empty().append(html);
+                $('.page-item.prev').addClass("disabled")
+
+                $(".page-item.item:first").addClass("active");
+            }
+
+            pagination.setClickEvent();
+        }  else{
+            //nothing
+        }
+    },
+    next : function () {
+        // current page group
+        var lastPage = Number($(".page-item.item:last").text());
+        var nextPage = lastPage + 1;
+        var rest = totalPageCount - lastPage;
+        var source = $('#page-template').html();
+        var template = Handlebars.compile(source);
+
+        if (rest > pagination.limit){
+            var html = template({start:nextPage, end:(nextPage + pagination.limit)});
+            $('#pageSection').empty().append(html);
+            $(".page-item.item:first").addClass("active");
+            var page = $(".page-item.item:first").text();
+
+
+        }else{
+            var html = template({start:nextPage,end:(nextPage + rest)});
+            $('#pageSection').empty().append(html);
+            $(".page-item.item:first").addClass("active");
+            $('.page-item.next').addClass("disabled");
+
+        }
+
+        return false;
+    },
+    prev : function(){
+        var firstPage = Number($(".page-item.item:first").text());
+        var start = firstPage - pagination.limit;
+
+        var source = $('#page-template').html();
+        var template = Handlebars.compile(source);
+
+        if (start == 1){
+            var html = template({start:1, end:pagination.limit});
+            $('#pageSection').empty().append(html);
+            $('.page-item.prev').addClass("disabled");
+        } else{
+            var html = template({start:start, end:(start + pagination.limit - 1)});
+            $('#pageSection').empty().append(html);
+        }
+
+
+        $(".page-item.item:first").addClass("active");
+
+        return false;
+    },
+    setClickEvent: function () {
+
+        $(document).on("click", "a.page-link.prev" , function(event) {
+            pagination.prev();
+
+        });
+
+
+        $(document).on("click", "a.page-link.next" , function(event) {
+            pagination.next();
+
+        });
+
+
+        $(document).on("click", "a.page-link.item" , function(event) {
+            var page = Number(event.target.text);
+            var first = Number($(".page-item.item:first").text());
+            var order = page - first;
+            $(".page-item.item").removeClass("active");
+            $(".page-item.item").eq(order).addClass("active");
+            main.getPage();
+
+
+        });
+    }
+}
